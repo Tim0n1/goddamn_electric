f9dae1fae3cf28caa89424c75de892137d60e516
timon <timonaki@abv.bg>
2022-07-07 22:36:14 +0300

    gambling implementation

--- musicbot/commands/DB.py
+++ musicbot/commands/DB.py
@@ -2,3 +2,33 @@ import mysql.connector
 db = mysql.connector.connect(host='sql11.freesqldatabase.com', user='sql11504349', passwd='WBXaQjYwhn', database='sql11504349')
 cursor = db.cursor()
 
+def get_all_users():
+    cursor.execute('SELECT * FROM user_info')
+    return cursor.fetchall()
+
+
+def get_all_ids():
+    cursor.execute('SELECT discord_id FROM user_info')
+    return cursor.fetchall()
+
+class BalanceUtilisation:
+
+    @staticmethod
+    def get_balance(discord_id: int):
+        for i in get_all_users():
+            if i[1] == discord_id:
+                return i[2]
+
+    @staticmethod
+    def new_balance(discord_id: int, new_balance: int):
+        cursor.execute('''UPDATE user
+                                SET 
+                                    balance = %s
+                                WHERE
+                                    discord_id = %s ;''', (new_balance, discord_id))
+        db.commit()
+
+    @staticmethod
+    def get_all_ids():
+        cursor.execute('SELECT discord_id FROM user_info')
+        return cursor.fetchall()
--- musicbot/commands/gambling_commands.py
+++ musicbot/commands/gambling_commands.py
@@ -1,9 +1,10 @@
 import discord
+import random
 from config import config
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 from musicbot.commands import DB
-
+from musicbot import gambling_utils
 
 class Gambling(commands.Cog):
     def __init__(self, bot):
@@ -11,10 +12,48 @@ class Gambling(commands.Cog):
 
     @commands.command(name='balance')
     async def _balance(self, ctx):
-        balance = 1000
-        DB.cursor.execute('INSERT INTO user_info (discord_id, balance) VALUES (%s,%s)', (ctx.message.author.id, balance))
-        DB.db.commit()
-        await ctx.send(f'{balance}lv were given to {ctx.message.author.mention}')
+        discord_id = ctx.message.author.id
+        if discord_id in DB.get_all_ids():
+            f'Current balance: {DB.BalanceUtilisation.get_balance(discord_id)}'
+        else:
+            balance = 1000
+            DB.cursor.execute('INSERT INTO user_info (discord_id, balance) VALUES (%s,%s)', (discord_id, balance))
+            DB.db.commit()
+            await ctx.send(f'{balance} leva. were given to {ctx.message.author.mention}. Current balance: {DB.BalanceUtilisation.get_balance(discord_id)}')
+
+    @commands.command(name='gamble')
+    async def _gamble(self, ctx, *, ammount, bet):
+        discord_id = ctx.message.author.id
+        if discord_id in DB.get_all_ids():
+            try:
+                new_balance = 0
+
+                balance = DB.BalanceUtilisation.get_balance(discord_id)
+                choice = random.choice(gambling_utils.wheel_numbers)
+                await ctx.send(file=discord.File(gambling_utils.get_wheel_number_path(choice)))
+                if bet == 'black' and choice in gambling_utils.wheel_numbers_black:
+                    new_balance = balance + ammount
+                    DB.BalanceUtilisation.new_balance(discord_id,new_balance)
+                    await ctx.send(f'{ctx.message.author.mention} won {new_balance}lv')
+                elif bet == 'red' and choice in gambling_utils.wheel_numbers_red:
+                    new_balance = balance + ammount
+                    DB.BalanceUtilisation.new_balance(discord_id, new_balance)
+                    await ctx.send(f'{ctx.message.author.mention} won {new_balance}lv')
+                elif bet == 'green' and choice == 0:
+                    new_balance = balance + 14 * ammount
+                    DB.BalanceUtilisation.new_balance(discord_id, new_balance)
+                    await ctx.send(f'{ctx.message.author.mention} won {new_balance}lv')
+                else:
+                    new_balance = balance - ammount
+                    DB.BalanceUtilisation.new_balance(discord_id, new_balance)
+                    await ctx.send(f'{ctx.message.author.mention} lost {new_balance}lv')
+
+            except Exception:
+                await ctx.send('Please type \'.gamble {ammount} {black/red/yellow}\'')
+        else:
+            await ctx.send('Type .balance to get your first 1000lv')
+
+
 
 
 def setup(bot):
