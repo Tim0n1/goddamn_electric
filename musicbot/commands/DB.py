fad8a38180559cdbf302ff15c7eab5a9af07f7e0
Tim0n1 <96350236+Tim0n1@users.noreply.github.com>
2022-07-14 20:41:15 +0300

    Update DB.py

--- musicbot/commands/DB.py
+++ musicbot/commands/DB.py
@@ -1,5 +1,5 @@
 import mysql.connector
-db = mysql.connector.connect(host='sql11.freesqldatabase.com', user='sql11504349', passwd='WBXaQjYwhn', database='sql11504349')
+db = mysql.connector.connect(host='sql11.freemysqlhosting.net', user='sql11506524', passwd='zKbAUcgDib', database='sql11506524')
 cursor = db.cursor()
 
 def get_all_users():
