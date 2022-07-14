190f9329c281337104fc3816e8da24209068705b
Tim0n1 <96350236+Tim0n1@users.noreply.github.com>
2022-07-14 21:08:17 +0300

    Update DB.py

--- musicbot/commands/DB.py
+++ musicbot/commands/DB.py
@@ -9,7 +9,7 @@ def get_all_users():
 
 def get_all_ids():
     cursor.execute('SELECT discord_id FROM user_info')
-    return cursor.fetchall()
+    return cursor.fetchall()[0]
 
 class BalanceUtilisation:
 
